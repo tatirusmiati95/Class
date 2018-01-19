3165c2776ff5a8a7de5158db914b45b07c222bbc
aulianadia51@gmail.com <32425968+tatirusmiati95@users.noreply.github.com>
2018-01-19 21:17:50 +0800

    membuat class motor.cpp

---
+++ class motor.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class motor{
+		public :
+			int jumlhmotor;
+			string merkmotor;
+			void berkendara(string kecepatan);
+};
+
+void motor::berkendara(string kecepatan){
+	
+	cout<< " Kecepatan Motor :"<< kecepatan << endl;
+}
+int main (){
+
+}
